4e457f30f4a12dd4374d4bfc88c76566a455e59a
jihyeon Hwang <jihyeon1804@gmail.com>
2021-08-06 08:47:39 +0900

    댓글 css랑 html 복붙 정도만 한 정도

--- static/js/del_btn.js
+++ static/js/del_btn.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-    $('#del_btn').on('click', function() {
+$(function(){
+    $('#del_btn').on('click', function(){
         alert('인식 좀 돼라')
-        if(confirm("정말로 삭제하시겠습니까?")) {
-            location.href = $(this).data('uri');
-        }
-    });
-});
+        // if(confirm("정말로 삭제하시겠습니까?")) {
+        //     location.href = $(this).data('uri')
+        // }
+    })
+})
---
+++ static/js/list_btn.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('#list_btn').on('click', function(){
+        alert('목록으로 진입')
+        document.location.href = '/bbs/list/'
+    })
+})
